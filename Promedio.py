8208247f467e392495d615eabe78e401fb384a8d
gastonruston <gastonruston@gmail.com>
2026-04-15 19:24:52 -0300

    Primer ejercicio

---
+++ Promedio.py
@@ -0,0 +1,6 @@
+# Promedio de tres números.
+num1 = int(input("Ingresar el primer número "))
+num2 = int(input("Ingresar el segundo número "))
+num3 = int(input("Ingresar el tercer número "))
+promedio = (num1+num2+num3)/3
+print(f"El promedio de {num1}, {num2},{num3} es {round(promedio)}")
